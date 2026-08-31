8cd6fbe71124456235009a50ae91721d1f7cc479
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-08-31 03:46:27 -0400

    docs: annotate AuthSession OpenAPI method field

--- erp-api/authsession/api/serializers.py
+++ erp-api/authsession/api/serializers.py
@@ -22,7 +22,7 @@ class AuthSessionSerializer(serializers.ModelSerializer):
         )
         read_only_fields = fields
 
-    async def get_is_current(self, auth_session):
+    async def get_is_current(self, auth_session) -> bool:
         request = self.context.get("request")
         return bool(
             request
