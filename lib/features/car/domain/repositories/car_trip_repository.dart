5b03adca6cfcf9b7ee57b73bccdf4bd98ec84959
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-02 21:05:14 -0400

    refactor(car): keep read repository contract stable

--- lib/features/car/domain/repositories/car_trip_repository.dart
+++ lib/features/car/domain/repositories/car_trip_repository.dart
@@ -1,6 +1,5 @@
 import '../entities/car_revision.dart';
 import '../entities/car_trip.dart';
-import '../entities/car_trip_deletion_result.dart';
 import '../entities/car_trip_filter.dart';
 import '../entities/car_trip_summary_view.dart';
 
@@ -16,5 +15,4 @@ abstract interface class CarTripRepository {
   Future<List<CarTripSummaryView>> getTripSummaries({CarTripFilter? filter});
   Future<List<CarRevision>> getRevisionsForTrip(int tripId);
   Future<CarRevision?> getRevision(int revisionId);
-  Future<CarTripDeletionResult> deleteTrip(int tripId);
 }
