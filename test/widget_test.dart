5217ab2dafe4256fa35ff3828d59cf03508d474e
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-02 21:40:52 -0400

    test(app): import widget types for root smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sales_erp/app/app.dart';
 import 'package:sales_erp/core/security/app_lock_controller.dart';
