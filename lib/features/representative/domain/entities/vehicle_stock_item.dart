cd1d3e2ff7e72952cdb83a3616353fb586f841b4
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 19:37:28 -0400

    feat(rep): add vehicle stock domain entity

---
+++ lib/features/representative/domain/entities/vehicle_stock_item.dart
@@ -0,0 +1,13 @@
+class VehicleStockItem {
+  final int productId;
+  final String productName;
+  final int quantity;
+  final String sellingPrice;
+
+  const VehicleStockItem({
+    required this.productId,
+    required this.productName,
+    required this.quantity,
+    required this.sellingPrice,
+  });
+}
