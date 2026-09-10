9eec9105a79bb3c8362adf24a1fbb1bcd63bda98
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 19:50:41 -0400

    feat(rep): add representative customer entity

---
+++ lib/features/representative/domain/entities/representative_customer.dart
@@ -0,0 +1,11 @@
+class RepresentativeCustomer {
+  final int id;
+  final String name;
+  final String phone;
+
+  const RepresentativeCustomer({
+    required this.id,
+    required this.name,
+    required this.phone,
+  });
+}
