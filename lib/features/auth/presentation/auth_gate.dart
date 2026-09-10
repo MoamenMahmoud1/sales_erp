73ceff834d880986cf550cf6058cb41106b5e44b
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 19:20:52 -0400

    feat(auth): enter permission-aware app shell after authentication

--- lib/features/auth/presentation/auth_gate.dart
+++ lib/features/auth/presentation/auth_gate.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/presentation/app_shell.dart';
 import '../../../core/repositories/app_services.dart';
 import '../../../core/theme/app_theme.dart';
-import '../../dashboard/presentation/dashboard_controller.dart';
-import '../../dashboard/presentation/dashboard_page.dart';
-import '../../products/domain/product_repository.dart';
 import 'auth_controller.dart';
 import 'login_page.dart';
 
 class AuthGate extends StatefulWidget {
   final AuthController controller;
   final AppThemeController themeController;
-  final ProductRepository? productRepository;
 
   const AuthGate({
     super.key,
     required this.controller,
     required this.themeController,
-    this.productRepository,
   });
 
   @override
@@ -25,14 +21,9 @@ class AuthGate extends StatefulWidget {
 }
 
 class _AuthGateState extends State<AuthGate> {
-  late final DashboardController _dashboardController;
-
   @override
   void initState() {
     super.initState();
-    _dashboardController = DashboardController(
-      repository: AppServices.instance.dashboardRepository,
-    );
     widget.controller.addListener(_refresh);
     AppServices.instance.dataMode.addListener(_onDataModeChanged);
     widget.controller.restoreSession();
@@ -42,7 +33,6 @@ class _AuthGateState extends State<AuthGate> {
   void dispose() {
     widget.controller.removeListener(_refresh);
     AppServices.instance.dataMode.removeListener(_onDataModeChanged);
-    _dashboardController.dispose();
     super.dispose();
   }
 
@@ -63,9 +53,16 @@ class _AuthGateState extends State<AuthGate> {
           body: Center(child: CircularProgressIndicator()),
         );
       case AuthStatus.authenticated:
-        return DashboardPage(
-          onNavigateTo: (_) {},
-          controller: _dashboardController,
+        final user = widget.controller.currentUser;
+        if (user == null) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        return AppShell(
+          user: user,
+          themeController: widget.themeController,
+          onLock: () {},
         );
       case AuthStatus.unauthenticated:
         return LoginPage(controller: widget.controller);
