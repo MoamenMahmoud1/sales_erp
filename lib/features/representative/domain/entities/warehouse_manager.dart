33c7eba452171064c1f03b2205e92eac46e573a1
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 19:37:33 -0400

    feat(rep): add warehouse manager entity

---
+++ lib/features/representative/domain/entities/warehouse_manager.dart
@@ -0,0 +1,11 @@
+class WarehouseManager {
+  final int id;
+  final String name;
+  final String username;
+
+  const WarehouseManager({
+    required this.id,
+    required this.name,
+    required this.username,
+  });
+}
